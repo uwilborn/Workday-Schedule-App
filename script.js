8bb5462a6839d608aa80b4de951378e5078d694c
Ursula Wilborn <wilbornu@yahoo.com>
2021-05-22 14:39:59 -0400

    Update with for loop

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ rowContainer.className = "time-block row"
 timeBlock.className = "hour col-md-1"
 textBlock.className = "description col-md-10"
 textButton.className = "saveBtn col-md-1"
-
-timeBlock.innerText = 9 + i+":00";
+console.log(rowContainer)
+timeBlock.innerText = 9 + i + ":00";
+// textBlock.addEventListener("click",inputEvent);
 
 rowContainer.appendChild(timeBlock);
 rowContainer.appendChild(textBlock);
@@ -26,3 +27,17 @@ rowContainer.appendChild(textButton);
 document.getElementById("TopContainer").appendChild(rowContainer);
 
 }
+
+// function inputEvent(event){
+//     message.textContent = ""
+//     // console.log("click");
+//     // console.log(event.target.innerText);
+//     if(event.target.innerText === challengeQuestions[questionIndex].answers.correct){
+//       questionIndex++
+//       getQuestion() 
+//     } else{ 
+//         //console.log("Wrong Answer")
+//       message.textContent = "Wrong Answer, click again. Time reduced by 3 seconds."
+//       quiz.append(message)
+//     secondsLeft = secondsLeft - 3;
+//     }
